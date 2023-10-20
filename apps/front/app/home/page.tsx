d987be4f5bc2331cda775cb7290d13f64cd73d11
EmmaGuillaume <emma.guillaume@mmibordeaux.com>
2023-10-20 12:06:10 +0200

    [STYLE] add tailwind variable, fonts and colors

--- apps/front/app/home/page.tsx
+++ apps/front/app/home/page.tsx
@@ -1,9 +1,55 @@
+"use client";
+import { useRef } from "react";
 import Image from "next/image";
 
 export default function HomeConnected() {
+  const videoRef = useRef<any>(null);
+
   return (
-    <main className="flex justify-center">
-      <div className="text-red-500">Connected, welcome </div>
+    <main className="flex flex-col justify-center bg-slate-200">
+      <div className="text-purple bg-blue-light1 font-raleway">
+        Connected, welcome{" "}
+      </div>
+      <div className="snap-y ">
+        {/* <video
+          className="snap-center w-full h-[850px]"
+          ref={videoRef}
+          onClick={() =>
+            videoRef.current.paused
+              ? videoRef.current.play()
+              : videoRef.current.pause()
+          }
+        >
+          <source
+            src="/video/mais-ca-veut-dire-quoi-ces-pourcentages-de-pluie-sur-les-applis-meteo.mp4"
+            type="video/mp4"
+          />
+          Download the
+          <a href="https://www.youtube.com/watch?v=KSUF9ihIVVI">WEBM</a>
+          or
+          <a href="https://www.youtube.com/watch?v=KSUF9ihIVVI">MP4</a>
+          video.
+        </video>
+        <video
+          className="snap-center w-full h-[850px]"
+          ref={videoRef}
+          onClick={() =>
+            videoRef.current.paused
+              ? videoRef.current.play()
+              : videoRef.current.pause()
+          }
+        >
+          <source
+            src="/video/mais-ca-veut-dire-quoi-ces-pourcentages-de-pluie-sur-les-applis-meteo.mp4"
+            type="video/mp4"
+          />
+          Download the
+          <a href="https://www.youtube.com/watch?v=KSUF9ihIVVI">WEBM</a>
+          or
+          <a href="https://www.youtube.com/watch?v=KSUF9ihIVVI">MP4</a>
+          video.
+        </video> */}
+      </div>
     </main>
   );
 }
--- apps/front/app/page.tsx
+++ apps/front/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex justify-center">
-      <div className="text-blue-500">hello world </div>
+      <div className="text-purple bg-blue-light1">hello world </div>
       <Link href="./auth">connecte toi</Link>
     </main>
   );
