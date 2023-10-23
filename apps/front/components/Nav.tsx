9e52e1da265f7920c9fde9b029975c9fdc37e1df
EmmaGuillaume <emma.guillaume@mmibordeaux.com>
2023-10-23 09:19:23 +0200

    [STYLE] add color variable

--- apps/front/components/Nav.tsx
+++ apps/front/components/Nav.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Nav() {
   return (
     <div className="absolute bottom-8 w-full px-4">
-      <div className="flex justify-between bg-yellow-light1 items-center w-full rounded-xl h-12 px-16">
+      <div className="flex justify-between bg-white items-center w-full rounded-xl h-12 px-16 drop-shadow-md">
         <p>cc</p>
         <p>c</p>
         <p>ccc</p>
